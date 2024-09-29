6605633db9f5f3b0f6983293cfcc2d5898c36e3f
Sergio Santos <sergi@victus>
2024-09-29 14:41:55 +0100

    query update_1

--- Proj_atmega128_3/Atmega128/Livraria/Inc/query.h
+++ Proj_atmega128_3/Atmega128/Livraria/Inc/query.h
@@ -3,7 +3,7 @@
 Author: Sergio Manuel Santos
 	<sergio.salazar.santos@gmail.com>
 License: GNU General Public License
-Hardware: Atmega
+Hardware: Atmega 128
 Update:	14/01/2024
 Comment: 
 	Simple tools
@@ -21,13 +21,6 @@ Comment:
 #define CLEAR_REG(REG, HBITS)	(REG &= ~HBITS)
 #define READ_BIT(REG, BIT)		((REG >> BIT) & 1)
 #define TOGGLE_REG(REG, HBITS)	(REG ^= HBITS)
-
-#ifndef DATA_SIZE
-	#define DATA_SIZE 8
-#endif
-#ifndef DATA_BITS
-	#define DATA_BITS 7
-#endif
 
 /*** Procedure and Function ToolSet ***/
 void set_reg(volatile uint8_t* reg, uint8_t hbits);
